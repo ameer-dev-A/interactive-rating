f580aa5b299120826c71380d6d67a602de7296cc
ameer-dev-A <ameer.ahmed641@yahoo.com>
2026-08-21 18:58:33 +0300

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,16 +1,25 @@
-  let btn = document.querySelectorAll('.btns button');
-     let sub = document.querySelector('.sub');
-      let all = document.querySelector('.all');
-       let thx = document.querySelector('.thx');
-     let res =document.querySelector('.res');
+let btn = document.querySelectorAll('.btns button');
+let sub = document.querySelector('.sub');
+let all = document.querySelector('.all');
+let thx = document.querySelector('.thx');
+let res = document.querySelector('.res');
 
-     btn.forEach((bt)=>{
-      bt.addEventListener('click',(e)=>{
-        e.target.style.background = "white";
-        res.innerHTML  = e.target.value ;
-      })
-     })
-     sub.onclick=()=>{
-       all.style.display="none";
-       thx.style.display="block";
-     }
+btn.forEach((bt) => {
+  bt.addEventListener('click', (e) => {
+
+    
+    btn.forEach((b) => {
+      b.style.background = "";
+    });
+    
+    e.target.style.background = "white";
+
+   
+    res.innerHTML = e.target.value;
+  });
+});
+
+sub.onclick = () => {
+  all.style.display = "none";
+  thx.style.display = "block";
+};
